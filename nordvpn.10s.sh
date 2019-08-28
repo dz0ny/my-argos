4458de552a1148416f27287925c47cd19b2b2308
Janez Troha <dz0ny@ubuntu.si>
2019-08-28 10:32:16 +0200

    Update nordvpn.10s.sh

--- nordvpn.10s.sh
+++ nordvpn.10s.sh
@@ -9,7 +9,8 @@ STATUS_CITY=$(echo $STATUS | awk '{for(i=1;i<=NF;i++) if ($i=="City:") print $(i
 STATUS_IP=$(echo $STATUS | awk '{for(i=1;i<=NF;i++) if ($i=="IP:") print $(i+1)}')
 STATUS_PROTOCOL=$(echo $STATUS | awk '{for(i=1;i<=NF;i++) if ($i=="protocol:") print $(i+1)}')
 STATUS_SERVER_NAME=$(echo $STATUS | awk '{for(i=1;i<=NF;i++) if ($i=="server:") print $(i+1)}')
-
+STATUS_UPTIME=$(nordvpn status | grep -m1 "Uptime:")
+STATUS_TRRX=$(nordvpn status | grep -m1 "Transfer:")
 
 # icon
 echo "🌈"
@@ -18,39 +19,36 @@ echo "🌈"
 echo "---"
 
 if [[ $STATUS_CONNECTED == "Connected" ]]; then
-	COUNTRY_FLAG=$(curl -s \
-	"https://public-us.opendatasoft.com/explore/dataset/country-flags/files/$(curl -s \
-	"https://public-us.opendatasoft.com/api/records/1.0/search/?dataset=country-flags&q=$STATUS_COUNTRY&facet=country" \
-	| jq -r '.records[0].fields.flag.id')/300/" | base64 -w 0)
-  echo "<span color='#54a546'>Connected to:</span> <b>$STATUS_COUNTRY</b> | image='$COUNTRY_FLAG'\
-    imageWidth=25"
-  echo "IP: <span color='#54a546'>$STATUS_IP</span>"
+
+  echo "<span color='green'>Connected to:</span> <b>$STATUS_COUNTRY</b>"
+  echo "<span color='green'>$STATUS_PROTOCOL: <b>$STATUS_IP</b></span>"
+  echo "<span color='green'>$STATUS_UPTIME</span>"
+  echo "<span color='green'>$STATUS_TRRX</span>"
+elif [[ $STATUS_CONNECTED == "Connecting" ]]; then
+  echo -e "<span color='orange'>Connecting</span>"
 else
   echo -e "<span color='red'>Disconnected</span>"
-  echo "IP: <span color='red'>$(curl -s ident.me)</span>"
+  echo "IP: <span color='red'>$STATUS_IP</span>"
 fi
 
 
 echo "---"
 
 if [[ $STATUS_CONNECTED == "Disconnected" ]]; then
-  echo "Quick connect | bash='nordvpn c && sleep 1' terminal=false refresh=true"
+  echo "Quick connect | bash='nordvpn c' terminal=false refresh=true"
 else
-  echo "Disconnect | bash='nordvpn d && sleep 1' terminal=false refresh=true"
+  echo "Disconnect | bash='nordvpn d' terminal=false refresh=true"
 fi
 
 echo "Dedicated"
-echo "--Germany | bash='nordvpn d ;nordvpn c de545 && sleep 5' terminal=false trim=true refresh=true"
-echo "--United States | bash='nordvpn d ;nordvpn c us2928 && sleep 5' terminal=false trim=true refresh=true"
+echo "--Germany | bash='nordvpn d ;nordvpn c de545' terminal=false trim=true refresh=true"
+echo "--United States | bash='nordvpn d ;nordvpn c us2928' terminal=false trim=true refresh=true"
 
 # Start country list submenu
 echo "Connect to"
 
 for country in $(nordvpn countries); do
   echo "--$(echo $country | awk '{gsub(/,/,""); gsub(/_/," "); print;}')\
-    | bash='nordvpn d ;nordvpn c $(echo $country | awk '{gsub(/,/,""); print}') && sleep 5' terminal=false\
+    | bash='nordvpn d ;nordvpn c $(echo $country | awk '{gsub(/,/,""); print}')' terminal=false\
       trim=true refresh=true"
 done
-# End of country list
-
-echo "---"
